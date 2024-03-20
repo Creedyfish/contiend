bd8a03bae2a2d5152d4f9453c90d7d67bb00fa40
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-21 01:39:31 +0800

    initial commit

---
+++ config/auth-config.ts
@@ -0,0 +1,51 @@
+import NextAuth from "next-auth"
+import {
+    getServerSession,
+    type DefaultSession,
+    type NextAuthOptions,
+  } from "next-auth";
+import { type Adapter } from "next-auth/adapters";
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import { db } from "@/config/db";
+
+const prisma = new PrismaClient()
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+export const authOptions : NextAuthOptions= {
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_ID ?? (() => { throw new Error("GOOGLE_ID is not set in .env file"); })(),
+      clientSecret: process.env.GOOGLE_SECRET ??(() => { throw new Error("GOOGLE_ClIENT is not set in .env file"); })(),
+    }),
+    // ...add more providers here
+  ],
+  adapter: PrismaAdapter(db) as Adapter,
+}
+
+export const getServerAuthSession = () => getServerSession(authOptions);
---
+++ providers/SessionProvider.tsx
@@ -0,0 +1,14 @@
+// import { SessionProvider } from "next-auth/react"
+
+// export default function App({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode,
+//   pageProps: { session, ...pageProps },
+// }) {
+//   return (
+//     <SessionProvider session={session}>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//   )
+// }
