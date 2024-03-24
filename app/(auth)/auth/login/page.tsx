66575a329bee2138a119e4b4ba9af2405ed52aa4
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-25 00:52:41 +0800

    feat: Add TabsDemo and Logo components to login page

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import { TabsDemo } from "@/components/TabsTest";
+import Logo from "@/components/logo";
 export default function page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex h-full w-full flex-grow bg-red-500 ">sadasd</div>
+    <main className="container mx-auto flex min-h-screen flex-col justify-between">
+      <div className=" flex w-full flex-1 items-center justify-center bg-red-500">
+        <div className="flex w-full bg-blue-400 md:w-4/5">
+          <div className="flex w-full flex-1 items-center justify-center bg-yellow-400"></div>
+          <div className="flex w-full flex-1 flex-col justify-center p-5">
+            <Logo />
+            <TabsDemo />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
